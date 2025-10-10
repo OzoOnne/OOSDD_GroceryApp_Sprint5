1211efa29b92a651b556a636d0cbfd76981e7fe9
OzoOnne <55187541+OzoOnne@users.noreply.github.com>
2025-10-10 11:52:44 +0200

    price added

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -8,11 +8,13 @@ namespace Grocery.Core.Data.Repositories
         private readonly List<Product> products;
         public ProductRepository()
         {
-            products = [
-                new Product(1, "Melk", 300, new DateOnly(2025, 9, 25)),
-                new Product(2, "Kaas", 100, new DateOnly(2025, 9, 30)),
-                new Product(3, "Brood", 400, new DateOnly(2025, 9, 12)),
-                new Product(4, "Cornflakes", 0, new DateOnly(2025, 12, 31))];
+            products =
+            [
+                new Product(1, "Melk", 300, 1.05m, new DateOnly(2025, 9, 25)),
+                new Product(2, "Kaas", 100, 2.95m, new DateOnly(2025, 9, 30)),
+                new Product(3, "Brood", 400, 1.25m, new DateOnly(2025, 9, 12)),
+                new Product(4, "Cornflakes", 0, 1.35m, new DateOnly(2025, 12, 31))
+            ];
         }
         public List<Product> GetAll()
         {
